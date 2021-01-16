96b9e592ff9d0b021c5a3556365229b8220ed39a
alvaro <alvaro.honrubia@upm.es>
2021-01-16 20:18:13 +0100

    errorHandlerSintax working

--- src/Errors/ErrorsHandler.ts
+++ src/Errors/ErrorsHandler.ts
@@ -7,21 +7,21 @@ import { Token } from '../Types/Types';
 // Constants
 import { erroresSintactico } from '../Assets/Params';
 
-export const errorHandlerSintax = (filaAccion: number, columnaAccion: number, token: Token) => {
+export const errorHandlerSintax = (estado: number, token: Token) => {
   if (token.posicion) {
     const error: string =
       '[ERROR SINTÁCTICO](Línea: ' +
       token.posicion.linea +
       ', Columna: ' +
       token.posicion.columna +
-      ") - No se esperaba el token: " + 
+      "): No se esperaba el token: " + 
       token.codigo +
-      " , "
-      erroresSintactico[0] +
+      " ; "
+      erroresSintactico[estado] +
       '\n';
     if (process.argv.includes('-sin')) {
       console.log(chalk.bgYellow(error));
     }
-    fs.appendFileSync('Errores.txt', error);
+    fs.appendFileSync('Output/Errores.txt', error);
   }
 };
